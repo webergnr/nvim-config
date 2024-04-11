a2188c1ebe47e7a28e347c3346bebd732b2816d4
Weber Grandizoli <weber@mail.com>
2024-04-11 12:59:26 -0300

    initial commit

---
+++ lua/plugins/manson.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server", "stylua",
+        "html-lsp", "css-lsp" , "prettier"
+      },
+    },
+  }
+}
+
